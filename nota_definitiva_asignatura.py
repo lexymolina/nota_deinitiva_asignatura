b140475c7bf82c11cc7302155cb1c3eb26a78b0b
sistemas <you@example.com>
2023-02-10 10:00:29 -0500

    se modifico el driagrama.drawio el diagrama.png y la nota definitiva.py

---
+++ nota_definitiva_asignatura.py
@@ -0,0 +1,21 @@
+# programa nota definitiva asignatura
+
+print("------------------------------")
+print("--nota definitiva asignatura--")
+print("------------------------------")
+
+#input
+nc = int(input("digite el valor de nc: "))
+cp = int(input("digite el valor de cp: "))
+na = int(input("digite el valor de na: "))
+au = int(input("digite el valor de au: "))
+bi = int(input("digite el valor de bi: "))
+
+#processing
+nd = ("0.3*nc + 0.3*np + 0.1*na + 0.1*au + 0.2*bi")
+
+#output
+print("--------------------")
+print("-----resultados-----")
+print("--------------------")
+
